02d28a3346507d419ef67c7eaea55fe090e38728
Alexis Zamanidis <alexiszamanidis@outlook.com>
2022-01-09 04:12:12 +0200

    add react-icons package

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { BsFileTextFill } from "react-icons/bs";
 
 const Title = styled.h1`
     color: red;
@@ -7,5 +9,12 @@ const Title = styled.h1`
 `;
 
 export default function Home() {
-    return <Title>My page</Title>;
+    return (
+        <div>
+            <FaGithub />
+            <FaLinkedin />
+            <BsFileTextFill />
+            <Title>My page</Title>;
+        </div>
+    );
 }
