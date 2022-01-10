84a1c32ebf22e479b821da58384534e55a8ea9ca
Alexis Zamanidis <alexiszamanidis@outlook.com>
2022-01-11 00:18:02 +0200

    clean up Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,13 +1,10 @@
 import SocialIcons from "../SocialIcons";
-import { fullName, summary } from "../../constants";
 
 const Header = () => {
     return (
-        <div>
-            <div>{fullName}</div>
-            <div>{summary}</div>
+        <>
             <SocialIcons />
-        </div>
+        </>
     );
 };
 
