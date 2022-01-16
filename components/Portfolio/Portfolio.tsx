90f5e1f9e2133ab9bed896cd403fd0ba5f6138e3
Alexis Zamanidis <alexiszamanidis@outlook.com>
2022-01-16 16:28:29 +0200

    add github spinner

--- components/Portfolio/Portfolio.tsx
+++ components/Portfolio/Portfolio.tsx
@@ -1,24 +1,28 @@
 import { FadeIn } from "../Styles";
 import AlertBox from "../AlertBox";
 import GitHubCard from "../GitHubCard";
-import { GitHubRepositories } from "./styles";
 import { GitHubRepository } from "../../types";
 import { githubUsername } from "../../constants";
 import { useGetUserRepositories } from "../../services/GitHubService";
+import { GitHubIconWrapper, GitHubRepositories, StyledGitHubIcon } from "./styles";
 
 const Portfolio = () => {
     const { repositories, isLoading, isError } = useGetUserRepositories(githubUsername);
 
     if (isError) return <AlertBox>Failed to fetch the repositories...</AlertBox>;
-    if (isLoading) return <div>loading...</div>;
 
     return (
         <FadeIn>
-            <GitHubRepositories>
-                {repositories.map((repository: GitHubRepository, index: number) => {
-                    return <GitHubCard key={index} {...repository} />;
-                })}
-            </GitHubRepositories>
+            <GitHubIconWrapper loading={isLoading.toString()}>
+                <StyledGitHubIcon />
+            </GitHubIconWrapper>
+            {!isLoading && (
+                <GitHubRepositories>
+                    {repositories.map((repository: GitHubRepository, index: number) => {
+                        return <GitHubCard key={index} {...repository} />;
+                    })}
+                </GitHubRepositories>
+            )}
         </FadeIn>
     );
 };
--- components/Portfolio/styles.tsx
+++ components/Portfolio/styles.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { FaGithub } from "react-icons/fa";
 
 export const GitHubRepositories = styled.div`
     padding: 70px;
@@ -13,4 +14,32 @@ export const GitHubRepositories = styled.div`
         width: 100%;
         padding: 70px 10px;
     }
+`;
+
+export const GitHubIconWrapper = styled.div<{ loading: string }>`
+    width: 50px;
+    height: 50px;
+    margin: 70px auto 0px;
+    ${({ loading }) =>
+        loading === "true" &&
+        `
+    animation-name: spin;
+    animation-duration: 5000ms;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+
+    @keyframes spin {
+        from {
+            transform: rotate(0deg);
+        }
+        to {
+            transform: rotate(360deg);
+        }
+    }
+    `}
+`;
+
+export const StyledGitHubIcon = styled(FaGithub)`
+    width: 50px;
+    height: 50px;
 `;
