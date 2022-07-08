f0fb45e24ae9de431dd217db948dae257086c6bf
alexiszamanidis <48658768+alexiszamanidis@users.noreply.github.com>
2022-07-08 19:58:36 +0000

    Deploying to gh-pages from  @ 3174f1c12f466925e7f807426628462ba7a736c1 🚀

---
+++ _next/static/cbgIztG-wvP4h1LHO6I3c/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d0447323-b5bd5a3bef8e2d33.js","static/chunks/d7eeaac4-97fd4fa5713688e8.js","static/chunks/pages/index-ce4dba974bb74fb9.js"],"/_error":["static/chunks/pages/_error-9734db4c9fd59614.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
